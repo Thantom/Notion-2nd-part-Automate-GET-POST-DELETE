4bd3b3ef2754c8f61bbacf9426029d0543fad70b
Thantom <nervashe@mail.ru>
2026-05-21 20:54:23 +0300

    Create playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+
+  testDir: './tests',
+
+  use: {
+    baseURL: 'https://api.restful-api.dev'
+  },
+
+  reporter: [['html']]
+});
